ce8727501a2a63ea34d6f9a924cb96db6221ac05
manuela-mendonça <manuelamendoncaf5@gmail.com>
2025-11-12 16:38:43 -0300

    12/11

---
+++ projeto/script.js
@@ -0,0 +1,14 @@
+const seletorCor = document.getElementById("seletorCor");
+const btnAplicarCor = document.getElementById("btnAplicarCor");
+
+btnAplicarCor.addEventListener("click", (e) => {
+    const cor = seletorCor.value;
+    document.documentElement.style.setProperty("--cor-fundo", cor);
+});
+
+
+const chips = document.querySelectorAll(".chip");
+const galeria = document.getElementById("galerihero");
+
+const imagemInicial = chips[0].dataset.imagem;
+galeria.style.backgroundImage = `url(${imagemInicial})`;
---
+++ script.js
@@ -0,0 +1,3 @@
+const titulo = document.getElementById('titulo')
+console.log(titulo)
+alert("o texto é:" + titulo)
